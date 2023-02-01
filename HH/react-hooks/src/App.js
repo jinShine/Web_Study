9d1ce73c16659fe72d508e996b6b8c9996457834
Seungjin Kim <seungjin429@gmail.com>
2023-02-01 23:59:30 +0900

    [HH] - Hooks정리

---
+++ HH/react-hooks/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Counter } from "./1_UseState";
+import { Info } from "./2_UseEffect";
+import { useState } from "react";
+import { UseReducerPrac } from "./3_UseReducer";
+import { UseMemoPrac } from "./4_UseMemo";
+
+function App() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setVisible(!visible)}>{visible ? "숨기기" : "보이기"}</button>
+      <hr></hr>
+      {visible && (
+        <div>
+          <Counter />
+          <Info />
+          <UseReducerPrac />
+          <UseMemoPrac />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
