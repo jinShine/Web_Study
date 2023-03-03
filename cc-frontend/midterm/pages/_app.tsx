50b040caf4cf2d8d4344a75445eb6962c9eb9080
Seungjin Kim <seungjin429@gmail.com>
2023-03-03 23:32:53 +0900

    [cc-frontend] 19일차 - 중간 프로젝트

---
+++ cc-frontend/midterm/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider, Global } from "@emotion/react";
+import Layout from "../src/components/commons/layout/";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import { theme } from "../src/commons/styles/globalTheme";
+import ApolloSetting from "../src/components/commons/apollo";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloSetting>
+      <>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </>
+    </ApolloSetting>
+  );
+}
