ad6446b240330d49eb13a30dd5912231b69dee71
Seungjin Kim <seungjin429@gmail.com>
2023-03-13 01:36:25 +0900

    [learn-nextjs] - layout

---
+++ learn-nextjs-docs/nextjs-blog/src/constants/userDetail.js
@@ -0,0 +1,6 @@
+export const userDetail = {
+  name: 'buzz',
+  age: 33,
+  height: '2m',
+  weight: 50,
+}
---
+++ learn-nextjs-docs/nextjs-blog/src/pages/category/[slug].js
@@ -0,0 +1,26 @@
+import Layout from 'components/Layout'
+import SubLayout from 'components/SubLayout'
+import { useRouter } from 'next/router'
+
+export default function CategorySlug() {
+  const router = useRouter()
+  const { slug, from, age } = router.query
+
+  console.log(router.query)
+
+  return (
+    <>
+      <h1 className="title">
+        Category {slug} from {from} {age}
+      </h1>
+    </>
+  )
+}
+
+CategorySlug.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  )
+}
