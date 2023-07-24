b0e189d0fc0cc25d0406103966dc2ac5d708b5da
Seungjin Kim <seungjin429@gmail.com>
2023-07-24 23:58:34 +0900

    [js_algorithm] 완전탐색 평균

--- JS_알고리즘 풀이/test.js
+++ JS_알고리즘 풀이/test.js
@@ -1,41 +1,114 @@
-/**
-// 핑퐁 문제
-// 일련의 숫자가 있고, 이 숫자는 1씩 증가, 또는 감소한다. N 번째의 숫자가 있을 시에, 이 숫자가 7의 배수(7, 14, 21)이거나 7이란 숫자를 포함할 시에 (7, 17, 27) 방향을 바꾼다.
-// 즉, 1, 2, 3, 4, 5, 6, [7], 6, 5, 4, 3, 2, 1, [0], 1, 2, [3], 2, 1, 0, [-1], 0, 1
-// 과 같이 숫자는 진행한다. (첫번째 7은 7번째, 두번째 0은 14번째, 세번째 3은 17번째, 네번째 -1은 21번째)
+const languages = ["C", "SWIFT", "JAVA", "SWIFT", "JAVA", "JAVA", "R"];
+const scores = [
+  [65, 80, 90],
+  [46, 100, 70],
+  [91, 96, 59],
+  [89, 90, 61],
+  [0, 94, 75],
+  [38, 95, 47],
+  [50, 60, 90],
+];
 
-// 다음의 제약 하에 pingpong(x)의 함수를 작성하라. 예시의 인풋과 아웃풋은 다음과 같다.
-// pingpong(8) = 6
-// pingpong(22) = 0
-// pingpong(68) = 2
-// pingpong(100) = 2
+// const languages = ["A", "AAA", "AA"];
+// const scores = [
+//   [100, 50, 0, 30],
+//   [100, 50, 0, 25],
+//   [100, 50, 0, 30],
+// ];
 
-// [조건]
-// - for문 이용.
-// - Assignment 를 쓰지 말 것. 즉, 변수 할당을 하지 말 것.
-// - String을 쓰지 말 것.
- */
+// function solution(languages, scores) {
+//   const answer = [];
 
-// for loop
+//   const datas = {};
 
-// console.log("A");
-// setTimeout(() => console.log("B"), 1000);
-// setTimeout(() => console.log("C"), 0);
-// console.log("D");
+//   for (let i = 0; i < languages.length; i++) {
+//     const lan = languages[i];
+//     const score = scores[i];
 
-// const obj = {
-//   hello: function () {
-//     console.log(this);
-//   },
-// };
+//     if (!datas[lan]) {
+//       datas[lan] = { scores: score, count: 1 };
+//     } else {
+//       datas[lan].count += 1;
+//       datas[lan].scores = datas[lan].scores.map(
+//         (value, index) => value + score[index]
+//       );
+//     }
+//   }
 
-// obj.hello();
+//   let keys = Object.keys(datas);
 
-function a() {
-  console.log(b); // undefined 같지만 b는 function b
-  var b = "bbb";
-  console.log(b); // bbb
-  function b() {}
-  console.log(b); // b함수
+//   for (const lan of keys) {
+//     datas[lan].scores = datas[lan].scores.map(
+//       (value) => value / datas[lan].count
+//     );
+//   }
+
+//   for (let i = 0; i < scores[0].length; i++) {
+//     let maxAvg = 0;
+//     let maxCount = 1;
+//     let maxLan = "";
+
+//     for (const lan of keys) {
+//       const avg = datas[lan].scores[i];
+
+//       if (avg > maxAvg) {
+//         maxAvg = avg;
+//         maxCount = datas[lan].count;
+//         maxLan = lan;
+//       } else if (avg === maxAvg) {
+//         if (datas[lan].count > maxCount) {
+//           maxLan = lan;
+//         }
+
+//         if (datas[lan].count == maxCount) {
+//           maxLan = [maxLan, lan].sort()[1];
+//         }
+//       }
+//     }
+
+//     answer.push(maxLan);
+//   }
+
+//   return answer;
+// }
+
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+
+function solution(languages, scores) {
+  const answer = [];
+
+  const datas = {};
+
+  for (let i = 0; i < languages.length; i++) {
+    const lan = languages[i];
+    datas[lan] = { score: scores[i] };
+  }
+
+  console.log(datas);
+
+  return answer;
 }
-a();
+
+const result = solution(languages, scores);
+console.log(result); // 예시의 경우 ["SWIFT", "JAVA", "R"] 출력
