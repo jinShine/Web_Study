8fecde3f05455e92f87c282af8371c400977e33e
Seungjin Kim <seungjin429@gmail.com>
2023-08-13 23:28:35 +0900

    [TS] - class

---
+++ js&ts/typescript_ex/src/class.ts
@@ -0,0 +1,59 @@
+function FPerson(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+FPerson.prototype.sayHi = () => {
+  console.log("Hi");
+};
+
+class CPerson {
+  name: string;
+  age: number;
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHi() {
+    console.log("Hi");
+  }
+}
+
+const fPerson = new FPerson("fBuzz", 32);
+const cPerson = new CPerson("cBuzz", 32);
+
+class Developer extends CPerson {
+  skill: string;
+
+  constructor(name: string, age: number, skill: string) {
+    super(name, age);
+    this.skill = skill;
+  }
+
+  coding() {
+    console.log("Coding...", this.skill);
+  }
+}
+
+class WatherPurifier {
+  private waterAmount: number;
+
+  constructor(waterAmount: number) {
+    this.waterAmount = waterAmount;
+  }
+
+  wash() {
+    if (this.waterAmount > 0) {
+      console.log("정상");
+    } else {
+      console.log("오류");
+    }
+  }
+}
+
+const machine1 = new WatherPurifier(30);
+machine1.wash(); // 정상
+machine1.waterAmount = 0; // 이렇게 접근해서 오작동을 일으키면 안좋겠지
+machine1.wash(); // 오류
--- js_algorithm/test.js
+++ js_algorithm/test.js
@@ -1,123 +1,68 @@
-// 매달 k일은 아파트 관리비를 내는날입니다.
-// 만약 그 달의 k일이 주말(토요일, 일요일)이라면 관리비는 k일로부터 가장 가까운 평일에 냅니다.
-// 한 해의 시작일인 1월 1일에 해당하는 요일 day와 매달 관리비를 내야하는 날짜 k가 매개변수로 주어질 떄,
-// 그 해의 1월부터 12월까지 매달 k일이 평일이면 0을, 주말이면 1을 순서대로 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+const employee1 = { firstName: "Buzz1" };
+const employee2 = { firstName: "Buzz2" };
 
-// 제한사항
-// * 1월 1일에 해당하는 요일은 다음과 같이 숫자로 주어집니다.
-//   * 월요일=0, 화요일=1, 수요일=2, 목요일=3, 금요일=4, 토요일=5, 일요일=6
-// * 윤년과 공휴일은 고려하지 않는다.
-// * 각 달의 날짜 수는 1월부터 순서대로 [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-// 입출력 예
-// day는 6, k가 1일때 result는 [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
-// day는 6, k가 25일때 result는 [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0]
-
-// 이 예시에서 1월 1일은 일요일이고, 매달 1일에 관리비를 내야 합니다.
-// 1월 1일은 주말이므로 result 배열의 첫번째 요소는 1입니다.
-// 2월부터 12월까지 매달 1일에 해당하는 요일은 다음과 같습니다.
-
-// 2월 : 수요일
-// 3월 : 수요일
-// 4월 : 토요일
-// 5월 : 월요일
-// 6월 : 목요일
-// 7월 : 토요일
-// 8월 : 화요일
-// 9월 : 금요일
-// 10월 : 일요일
-// 11월 : 수요일
-// 12월 : 금요일
-
-// function solution(day, k) {
-//   const answer = [];
-//   const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-//   day = day + (k % 7) - 1;
-
-//   for (let month = 0; month < 12; month++) {
-//     const days = daysInMonth[month];
-
-//     if (month === 0) {
-//       if (day === 5 || day === 6) {
-//         answer.push(1);
-//       } else {
-//         answer.push(0);
-//       }
-
-//       day += days % 7;
-
-//       continue;
-//     }
-
-//     if (day < 7) day += 7;
-
-//     let currentDay = day - 7;
-
-//     if (currentDay === 5 || currentDay === 6) {
-//       answer.push(1);
-//     } else {
-//       answer.push(0);
-//     }
-
-//     day = currentDay + (days % 7);
-//   }
-
-//   return answer;
-// }
-
-// // 예시 입력에 대한 출력
-// console.log(solution(6, 1)); // [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
-// console.log(solution(6, 25)); // [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0]
-
-// function solution(a, b, budget) {
-//   var answer = 0;
-
-//   for (let i = 0; i * a < budget; i++) {
-//     for (let j = 0; j * b < budget; j++) {
-//       if (budget - (i * a + j * b) === 0) {
-//         answer += 1;
-//       }
-//     }
-//   }
-
-//   return answer;
-// }
-
-// console.log(solution(3000, 5000, 23000));
-
-function solution(orders) {
-  const answer = [];
-  const user = {};
-
-  orders.forEach((order) => {
-    const arr = order.split(" ");
-    if (user[arr[0]] == undefined) user[arr[0]] = [...arr];
-    else user[arr[0]] = [...user[arr[0]], ...arr];
-  });
-
-  for (key in user) {
-    user[key] = [...new Set(user[key])];
-    answer.push(user[key]);
-  }
-
-  let max = Math.max.apply(
-    Math,
-    answer.map((el) => el.length)
-  );
-
-  return answer.filter((data) => data.length === max).map((data) => data[0]);
-
-  // return answer;
+function invite(greeting1) {
+  console.log(greeting1 + " " + this.firstName + " ");
 }
 
-console.log(
-  solution([
-    "alex pizza pasta",
-    "alex pizza pizza",
-    "alex noodle",
-    "bob pasta",
-    "bob noodle sandwich pasta",
-    "bob steak noodle",
-  ])
-);
+invite.call(employee1, "Hello~");
+invite.call(employee2, "Hello!!!");
+
+invite.apply(employee1, ["apply~~~1", "apply~~~2"]);
+
+const inviteEmployee = invite.bind(employee1);
+inviteEmployee("bindHello");
+
+console.log(JSON.stringify(employee1));
+
+const parsed = JSON.stringify(employee1);
+console.log(JSON.parse(parsed));
+
+let arrayInteger = [1, 2, 3, 4, 5, 6];
+const ddd = arrayInteger.slice(0, 3); // 새로운 배열
+const ddd1 = arrayInteger.slice(3);
+console.log(ddd);
+console.log(ddd1);
+
+let arrayIntegersOriginal1 = [1, 2, 3, 4, 5];
+let arrayIntegersOriginal2 = [1, 2, 3, 4, 5];
+let arrayIntegersOriginal3 = [1, 2, 3, 4, 5];
+
+let splicedArray1 = arrayIntegersOriginal1.splice(3, 1, "ㅋ", "222");
+// let splicedArray1 = arrayIntegersOriginal1.splice(3);
+
+console.log(splicedArray1);
+console.log(arrayIntegersOriginal1);
+
+{
+  console.log("!@#!@#!@#");
+}
+
+let uri = "employeeDetails?name=john&occupation=manager";
+let encoded_uri = encodeURIComponent(uri);
+// encodeURI(uri);
+
+console.log("!@#!@#!@#", encoded_uri);
+let decoded_uri = decodeURIComponent(encoded_uri);
+// let decoded_uri = decodeURI()
+console.log("!@#!@#!@#", decoded_uri);
+
+const memorized = () => {
+  let cache = {};
+
+  return (value) => {
+    if (value in cache) {
+      console.log("캐시에서 가져옴");
+      return cache[value];
+    } else {
+      console.log("캐시에 저장 후 가져옴");
+      let result = value + 20;
+      cache[value] = result;
+      return result;
+    }
+  };
+};
+
+const memo = memorized();
+console.log(memo(20));
+console.log(memo(20));
