431411d0e36e69132d12a5243121d1db4b557451
Seungjin Kim <seungjin429@gmail.com>
2023-08-17 13:39:18 +0900

    [dc] - tailwind css

---
+++ react&next/dream_coding/tailwind/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-9xl">안녕</h1>
+      <button className="bg-blue-300 rounded-lg">버튼</button>
+    </div>
+  );
+}
+
+export default App;
