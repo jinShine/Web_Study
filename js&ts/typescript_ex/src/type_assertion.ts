015fcb4dbe9a8a5ae669f07697bc7e4e2652245d
Seungjin Kim <seungjin429@gmail.com>
2023-08-21 23:59:27 +0900

    [TS] - type assertion

--- js&ts/typescript_ex/src/type_assertion.ts
+++ js&ts/typescript_ex/src/type_assertion.ts
@@ -9,10 +9,10 @@ buzz.age = 32;
 
 console.log(buzz);
 
-type TPerson {
-  name: string,
-  age: number
-}
+type TPerson = {
+  name: string;
+  age: number;
+};
 
-const buzz2 = {} as TPerson
-buzz2.name = "버즈2"
+const buzz2 = {} as TPerson;
+buzz2.name = "버즈2";
