79aa3286b4b20b06295d2c7a9878a550b7dd0e3f
jinShine <seungjin429@gmail.com>
2023-11-28 09:45:03 +0900

    [js] - 함수형_JS_map

---
+++ js&ts/javascript_ex/functional/3. 제너레이터와 이터레이터/3-3. map.js
@@ -0,0 +1,36 @@
+const products = [
+  { name: "반팔티", price: 15000 },
+  { name: "긴팔티", price: 20000 },
+  { name: "핸드폰케이스", price: 15000 },
+  { name: "후드티", price: 30000 },
+  { name: "바지", price: 25000 },
+];
+
+let names = [];
+for (const p of products) {
+  names.push(p.name);
+}
+// console.log(names);
+
+const map1 = () => {
+  let names = [];
+  for (const p of products) {
+    names.push(p.name);
+  }
+  return names;
+};
+
+// console.log(map1());
+
+const map2 = (f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+};
+
+const resultNames = map2((p) => p.name, products);
+console.log(resultNames);
+const resultPrices = map2((p) => p.price, products);
+console.log(resultPrices);
