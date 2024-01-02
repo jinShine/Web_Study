3783c7968b0ad125d025d608b157f523a1b9028d
jinShine <seungjin429@gmail.com>
2024-01-02 23:42:44 +0900

    [cc-backend] - section 06

--- backend/cc/class/section06/06-06-express-with-OOP-MVC/index.js
+++ backend/cc/class/section06/06-06-express-with-OOP-MVC/index.js
@@ -1,16 +1,14 @@
 import express from "express";
 
-import { CashService } from "./cash.js";
-import { ProductService } from "./product.js";
+import { CashService } from "./mvc/controller/services/cash.service.js";
+import { ProductService } from "./mvc/controller/services/product.service.js";
 import { ProductController } from "./mvc/controller/product.controller.js";
 
 const app = express();
 
 const productController = new ProductController();
-
 // 상품 구매하기 API
 app.post("/products/buy", productController.buyProduct);
-
 // 상품 환불하기 API
 app.post("/products/refund", productController.refundProduct);
 
