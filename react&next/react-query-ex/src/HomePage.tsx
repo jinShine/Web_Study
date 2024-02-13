3a41b59a400a7443c283fccc4f4d6381d2dd5f77
jinShine <seungjin429@gmail.com>
2024-02-13 23:58:48 +0900

    [react] react query 정리

--- react&next/react-query-ex/src/HomePage.tsx
+++ react&next/react-query-ex/src/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPosts } from "./api";
+import { getPosts, getPostsByUsername } from "./api";
 
 export default function HomePage() {
   const {
@@ -14,11 +14,22 @@ export default function HomePage() {
   } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
+    staleTime: 60 * 1000, //  1분
+    gcTime: 60 * 1000 * 10, // 10분
   });
 
   console.log(data);
   console.log(status);
   console.log(fetchStatus);
+
+  const username = "Buzz";
+
+  const { data: postsDataByUsername } = useQuery({
+    queryKey: ["posts", username],
+    queryFn: () => getPostsByUsername(username),
+  });
+
+  console.log(postsDataByUsername);
 
   return <div>홈페이지</div>;
 }
