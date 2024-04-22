3a1fad1807dd2ab0c4255ae77edf1e13e25062e8
jinShine <seungjin429@gmail.com>
2024-04-22 23:53:50 +0900

    [nextjs] - carousel ui 추가

--- react_a_next/next-yt-music/yt-music/app/(site)/components/Category.tsx
+++ react_a_next/next-yt-music/yt-music/app/(site)/components/Category.tsx
@@ -5,10 +5,6 @@ import React from "react";
 import useUIState from "@/hooks/useUIState";
 import { homeCategoryList } from "@/lib/dummyData";
 import { cn } from "@/lib/utils";
-import Lottie from "lottie-react";
-import Rabbit from "./Rabbit.json";
-import Rabbit_1 from "./Rabbit test_1.json";
-import Robot from "./Robot.json";
 
 const Category = () => {
   const { homeCategory, setHomeCategory, setHeaderImageSrc } = useUIState();
@@ -24,27 +20,20 @@ const Category = () => {
   };
 
   return (
-    <>
-      <div className="w-[300px] h-[300px]">
-        <Lottie animationData={Rabbit_1} loop={true} width={10} height={10} />
-        {/* <Lottie animationData={Robot} loop={true} width={10} height={10} /> */}
-      </div>
-      {/* <ul className="max-w-full overflow-x-auto flex flex-row gap-4">
-        {homeCategoryList.map((item) => (
-          <li
-            key={item.label}
-            className={cn(
-              "h-[38px] min-w-fit px-3 flex justify-center items-center border-transparent rounded-lg bg-[rgba(144,144,144,0.45)] cursor-pointer",
-              item.label === homeCategory &&
-                "bg-white text-black  hover:bg-white"
-            )}
-            onClick={() => onClickCategory(item)}
-          >
-            {item.label}
-          </li>
-        ))}
-      </ul> */}
-    </>
+    <ul className="max-w-full overflow-x-auto flex flex-row gap-4">
+      {homeCategoryList.map((item) => (
+        <li
+          key={item.label}
+          className={cn(
+            "h-[38px] min-w-fit px-3 flex justify-center items-center border-transparent rounded-lg bg-[rgba(144,144,144,0.45)] cursor-pointer",
+            item.label === homeCategory && "bg-white text-black  hover:bg-white"
+          )}
+          onClick={() => onClickCategory(item)}
+        >
+          {item.label}
+        </li>
+      ))}
+    </ul>
   );
 };
 
