61e798658a3631504070e808f0a2c7da710f580a
jinShine <seungjin429@gmail.com>
2026-01-08 16:01:35 +0900

    feat: 문자열 패턴 알고리즘 추가

--- algorithm/자바스크립트_코딩테스트(패턴공략책)/1.문자열패턴.ts
+++ algorithm/자바스크립트_코딩테스트(패턴공략책)/1.문자열패턴.ts
@@ -1,4 +1,6 @@
-// ** 문자열 순서 뒤집기
+/**
+ * 문자열 순서 뒤집기
+ */
 // 방법 1: 내장 메서드 사용
 function reverseString1(target: string) {
   const result = Array.from(target).reverse().join("");
@@ -25,8 +27,9 @@ function reverseString2(target: string) {
 }
 reverseString2("hello");
 
-// ** 중복 제거
-// 방법 1: Set 사용
+/**
+ * 중복 제거
+ */
 function removeDuplicates(str: string) {
   const result = new Set([...str]);
   // ⭐️ Set을 배열로 변환하는 여러 방법들:
@@ -37,7 +40,9 @@ function removeDuplicates(str: string) {
 }
 removeDuplicates("abcdeefgg");
 
-// ** 문자열 압축
+/**
+ * 문자열 압축
+ */
 function compressString(str: string) {
   let result = "";
   let counter = 1;
@@ -62,3 +67,110 @@ console.log(compressString("aabcccccaaa"));
 console.log(compressString("aaabccdede"));
 compressString("aabcccccaaa"); // a2b1c5a3
 compressString("aaabccdede"); // aaabccdede -> 원형 문자열보다 길면 원형 문자열 반열
+
+/**
+ * 가장 많이 등장하는 단어
+ */
+function mostCommonWord(str: string) {
+  const words = str.toLowerCase().split(/\W+/);
+
+  const wordsMap = new Map<string, number>();
+
+  words.forEach((word) => {
+    if (wordsMap.has(word)) {
+      wordsMap.set(word, (wordsMap.get(word) ?? 0) + 1);
+    } else {
+      wordsMap.set(word, 1);
+    }
+  });
+
+  const maxValue = Math.max(...wordsMap.values());
+  return [...wordsMap].find(([key, value]) => value === maxValue)?.[0];
+}
+
+console.log(
+  mostCommonWord("Hello world! this is a test string, hello hello test")
+);
+
+/**
+ * 회문 (palindrome)
+ * - 앞에서 읽어도 뒤에서 읽어도 같은 결과를 나타내는 문자열
+ */
+
+// 1. 방법: reverse
+function isPalindromeWithReverse(str: string) {
+  return str === [...str].reverse().join("");
+}
+
+console.log(isPalindromeWithReverse("level")); // true
+console.log(isPalindromeWithReverse("soccer")); // false
+console.log(isPalindromeWithReverse("abba")); // true
+
+function isPalindromeWithLoop(str: string) {
+  const length = str.length;
+
+  const mid = Math.floor(length / 2);
+  for (let i = 0; i < mid; i++) {
+    if (str[i] !== str[length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPalindromeWithLoop("level")); // true
+console.log(isPalindromeWithLoop("soccer")); // false
+console.log(isPalindromeWithLoop("abba")); // true
+
+/**
+ * 애너그램
+ * - 단어의 문자를 재배열하여 다른 단어를 만드는것
+ * 예시)
+ *    - listen, silent => true
+ *    - mouse, moose => false
+ */
+
+function isAnagramWithSort(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const sortedStr1 = [...str1].sort().join("");
+  const sortedStr2 = [...str2].sort().join("");
+
+  return sortedStr1 === sortedStr2;
+}
+
+console.log("isAnagramWithSort", isAnagramWithSort("listen", "silent"));
+console.log("isAnagramWithSort", isAnagramWithSort("mouse", "moose"));
+
+function isAnagramWithMap(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const charMap = new Map<string, number>();
+
+  for (let char of str1) {
+    if (charMap.has(char)) {
+      charMap.set(char, (charMap.get(char) || 0) + 1);
+    } else {
+      charMap.set(char, 1);
+    }
+  }
+
+  for (let char of str2) {
+    if ((charMap.has(char) && charMap.get(char)) || 0 > 0) {
+      charMap.set(char, (charMap.get(char) || 0) - 1);
+    } else {
+      // charMap에 없거나 value가 0이면 애너그램이 아니다!
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log("isAnagramWithMap", isAnagramWithMap("listen", "silent"));
+console.log("isAnagramWithMap", isAnagramWithMap("mouse", "moose"));
