27c089b1ad8552478560926ea2171c424958b7f1
jinShine <seungjin429@gmail.com>
2026-01-13 23:15:16 +0900

    feat: 자바실습 01/13

---
+++ groom/java-study/src/main/java/org/example/g01_13/Assignment.java
@@ -0,0 +1,42 @@
+package org.example.g01_13;
+
+import java.util.Scanner;
+
+public class Assignment {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("첫번째 숫자를 입력하세요. :");
+    double num1 = scanner.nextDouble();
+
+    System.out.println("연산자를 입력하세요. (예 +, -, *, /, %) :");
+    String operator = scanner.next();
+
+    System.out.println("두번째 숫자를 입력하세요. :");
+    double num2 = scanner.nextDouble();
+
+    double result = 0;
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        System.out.println("잘못된 연산자입니다.");
+        scanner.close();
+        break;
+    }
+
+    System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
+    scanner.close();
+  }
+}
