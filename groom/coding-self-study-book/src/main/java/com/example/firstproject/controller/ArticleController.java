1a8d1bfa8bb6db93da332ec26fb540a68e86ffb1
jinShine <seungjin429@gmail.com>
2026-02-06 23:59:42 +0900

    feat: 스터디

---
+++ groom/coding-self-study-book/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,68 @@
+package com.example.firstproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        // 1. DTO를 Entity로 변환
+        Article article = form.toEntity();
+
+        log.info("article : " + article);
+
+        // 2. Repository를 사용해 Entity를 DB에 저장
+        Article saved = articleRepository.save(article);
+
+        log.info("saved : " + saved);
+
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable(name = "id") Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 데이터 조회하기
+        // Optional<Article> article = articleRepository.findById(id);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 조회된 데이터를 모델에 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articleList", articles);
+
+        return "articles/index";
+    }
+
+}
--- groom/my-spring-study-diary/src/main/java/com/study/my_spring_study_diary/controller/StudyLogController.java
+++ groom/my-spring-study-diary/src/main/java/com/study/my_spring_study_diary/controller/StudyLogController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.study.my_spring_study_diary.common.ApiResponse;
 import com.study.my_spring_study_diary.dto.request.StudyLogCreateRequest;
 import com.study.my_spring_study_diary.dto.response.StudyLogResponse;
