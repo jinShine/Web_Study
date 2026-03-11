1d5fefabe2f93a530a04c2d7ec7bd4cc94f2d89f
FSL-Buzz <buzz.kim@forspacelab.com>
2026-03-11 20:09:41 +0900

    feat: 아카이브 정리

--- code-camp/frontend/react-native/class-mobile/components/seciton01/01-03-webview/index.tsx
+++
@@ -1,5 +0,0 @@
-import WebView from "react-native-webview";
-
-export default function WebViewPage() {
-  return <WebView source={{ uri: "https://www.naver.com/" }} />;
-}
--- code-camp/frontend/react-native/class-web/src/commons/libraries/03-01-webview-log.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export const webviewlog = (message: string) => {
-  console.log(message);
-};
